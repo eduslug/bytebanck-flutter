bd438d8f3000546d822da42c4da53e7b8e9ea33f
eduslug <eduslug93@gmail.com>
2023-10-31 10:45:09 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+      home: Scaffold(
+    appBar: AppBar(
+      title: Text('transferencia'),
+    ),
+    floatingActionButton: FloatingActionButton(
+      onPressed: () {},
+      child: Icon(Icons.add),
+    ),
+  )));
+}
