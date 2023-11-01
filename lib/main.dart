36a3d08a8c2399c9f4b6e70ad867c0f4aa9831ab
eduslug <eduslug93@gmail.com>
2023-11-01 11:40:39 -0300

    create child list

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,26 @@ void main() {
   runApp(MaterialApp(
       home: Scaffold(
     appBar: AppBar(
-      title: Text('transferencia'),
+      title: Text('transferencia de crédito'),
     ),
+    body: Column(children: <Widget>[
+      Card(
+        child: ListTile(
+          title: Text('100.00'),
+          subtitle: Text('1000'),
+          tileColor: Color.fromARGB(255, 91, 129, 81),
+          leading: Icon(Icons.monetization_on),
+        ),
+      ),
+      Card(
+        child: ListTile(
+          title: Text('200.00'),
+          subtitle: Text('1000'),
+          tileColor: Color.fromARGB(255, 91, 129, 81),
+          leading: Icon(Icons.monetization_on),
+        ),
+      ),
+    ]),
     floatingActionButton: FloatingActionButton(
       onPressed: () {},
       child: Icon(Icons.add),
