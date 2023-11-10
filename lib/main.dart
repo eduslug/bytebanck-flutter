88e25ab824c676a8b2ea7b805186796804e69562
eduslug <eduslug93@gmail.com>
2023-11-09 23:13:43 -0300

    refatoracao

--- lib/main.dart
+++ lib/main.dart
@@ -6,27 +6,47 @@ void main() {
     appBar: AppBar(
       title: Text('transferencia de crédito'),
     ),
-    body: Column(children: <Widget>[
-      Card(
-        child: ListTile(
-          title: Text('100.00'),
-          subtitle: Text('1000'),
-          tileColor: Color.fromARGB(255, 91, 129, 81),
-          leading: Icon(Icons.monetization_on),
-        ),
-      ),
-      Card(
-        child: ListTile(
-          title: Text('200.00'),
-          subtitle: Text('1000'),
-          tileColor: Color.fromARGB(255, 91, 129, 81),
-          leading: Icon(Icons.monetization_on),
-        ),
-      ),
-    ]),
+    body: ListaTrasferencia(),
     floatingActionButton: FloatingActionButton(
       onPressed: () {},
       child: Icon(Icons.add),
     ),
   )));
+}
+
+class ListaTrasferencia extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Column(children: <Widget>[
+      ItensTransferencias(Transferencia(100.0, 1000)),
+      ItensTransferencias(Transferencia(2220, 5555)),
+    ]);
+  }
+}
+
+class ItensTransferencias extends StatelessWidget {
+  final Transferencia _transferencia;
+
+  ItensTransferencias(this._transferencia);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Card(
+      child: ListTile(
+        title: Text(_transferencia.valor.toString()),
+        subtitle: Text(_transferencia.numeroConta.toString()),
+        tileColor: Color.fromARGB(255, 91, 129, 81),
+        leading: Icon(Icons.monetization_on),
+      ),
+    );
+  }
+}
+
+class Transferencia {
+  final double valor;
+  final int numeroConta;
+
+  Transferencia(this.valor, this.numeroConta);
 }
